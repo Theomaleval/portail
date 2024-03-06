268cb2ad12969022b7f8d50a0088eb947544312e
Theomaleval <91140488+Theomaleval@users.noreply.github.com>
2024-03-06 16:37:22 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,12 +1,28 @@
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="light-drak-style.css">
+
+
+<div class="top-band"></div>
+
+
+
+</div class="rectangle">
+    <label>
+        <input type="checkbox" name="checkbox" class="toggle-checkbox" />
+        <div class="toggle-slot">
+            <div class="toggle-button"></div>
+        </div>
+    </label>
+
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                
                 <?php if (isset($_SESSION['LOGGED_USER'])) : ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Déconnexionnn</a>
-                    </li>
+                    <a class="nav-link btn btn-logout custom-btn" href="logout.php">Déconnexion</a>
+                    <!-- Ajoutez la classe "custom-btn" pour une personnalisation supplémentaire -->
                 <?php endif; ?>
             </ul>
         </div>
