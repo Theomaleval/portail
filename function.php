757bc47fc7a3f948c8fc62d99ab02bbc41daeee3
Theomaleval <91140488+Theomaleval@users.noreply.github.com>
2024-03-06 16:36:07 +0100

    Update function.php

--- function.php
+++ function.php
@@ -11,30 +11,6 @@ function displayAuthor(string $authorEmail, array $users): string
     return 'Auteur inconnu';
 }
 
-function isValidRecipe(array $recipe): bool
-{
-    if (array_key_exists('is_enabled', $recipe)) {
-        $isEnabled = $recipe['is_enabled'];
-    } else {
-        $isEnabled = false;
-    }
-
-    return $isEnabled;
-}
-
-function getRecipes(array $recipes): array
-{
-    $valid_recipes = [];
-
-    foreach ($recipes as $recipe) {
-        if (isValidRecipe($recipe)) {
-            $valid_recipes[] = $recipe;
-        }
-    }
-
-    return $valid_recipes;
-}
-
 function redirectToUrl(string $url): never
 {
     header("Location: {$url}");
